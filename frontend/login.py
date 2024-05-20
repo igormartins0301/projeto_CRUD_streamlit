cbf9159755de4020d13819122c09dd967ddfee3e
Igor Martins <igormartins0301@gmail.com>
2024-05-20 19:09:05 -0300

    login e paginas do streamlit part 1

--- frontend/login.py
+++ frontend/login.py
@@ -1,33 +1,33 @@
 import streamlit as st
 import requests
 
-# Função para fazer a requisição à API
-def fazer_requisicao(email, senha):
-    url = 'https://exemplo.com/api/login'
-    payload = {'email': email, 'password': senha}
-    response = requests.post(url, data=payload)
+def fazer_requisicao(form_data):
+    url = 'http://127.0.0.1:8000/login/access-token/'
+    response = requests.post(url, data=form_data)
+    
     if response.status_code == 200:
         return response.json().get('access_token')
     else:
         return None
 
-@st.cache
-def salvar_token(token):
-    return token
 
-# Configuração da página
-st.title('Bem-vindo!')
+def show_login():
+    st.title('Bem-vindo!')
 
-# Inputs para email e senha
-email = st.text_input('Email')
-senha = st.text_input('Senha', type='password')
+    with st.form(key='login_form'):
+        email = st.text_input('Email')
+        senha = st.text_input('Senha', type='password')
+        submit_button = st.form_submit_button('Entrar')
 
-# Botão de entrar
-if st.button('Entrar'):
-    token = fazer_requisicao(email, senha)
-    # Salvar o token
-    if token:
-        salvar_token(token)
-        st.success('Login bem-sucedido! Token salvo no cache.')
-    else:
-        st.error('Credenciais inválidas. Tente novamente.')
+    if submit_button:
+        form_data = {
+            'username': email,
+            'password': senha
+        }
+        token = fazer_requisicao(form_data)
+        
+        if token:
+            st.session_state.token = token
+            st.rerun()
+        else:
+            st.error('Credenciais inválidas. Tente novamente.')
